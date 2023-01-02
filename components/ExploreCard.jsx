b9c316fbf23c8b15a7fb10f0ca997f2ecb3c0ccd
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-02 11:57:39 +0700

    Finalizing ExploreCard :hammer:

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -11,6 +11,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     className={`relative ${
       active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.75] ease-out cursor-pointer`}
+    onClick={() => handleClick(id)}
   >
     <img
       src={imgUrl}
@@ -18,7 +19,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom lg:rotate-[-90deg] lg:origin-[0,0]">
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
       </h3>
     ) : (
@@ -35,6 +36,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
           Enter the Metaverse
         </p>
+        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+          {title}
+        </h2>
       </div>
     )}
   </motion.div>
