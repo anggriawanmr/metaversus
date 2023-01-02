f68d55e7efec2aaac65d87962aaa999098f0c969
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-02 12:01:37 +0700

    Editing GetStarted component :hammer:

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,23 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { fadeIn, planetVariants, staggerContainer } from '../utils/motion';
+import { startingFeatures } from '../constants';
+
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div></motion.div>
+    </motion.div>
   </section>
 );
 
