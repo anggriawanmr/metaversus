12c644fed6fdc768a891179be3090f20c9d8a559
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-02 12:05:58 +0700

    Editing GetStarted component :hammer:

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -16,7 +16,23 @@ const GetStarted = () => (
       viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
-      <motion.div></motion.div>
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| How Metaversus Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
+      </motion.div>
     </motion.div>
   </section>
 );
