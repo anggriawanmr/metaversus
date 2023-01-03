20256ba06cb03587fe68f114c2ed4f9f03d3e16b
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-03 14:47:27 +0700

    Adding import files to Insights component :sparkles:

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { Insights } from '../constants';
+
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    ></motion.div>
   </section>
 );
 
