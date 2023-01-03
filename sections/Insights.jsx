58128a8cb52c19af9dce81bbf54cb05bd7ce63c8
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-03 14:52:21 +0700

    Editing Insights component :hammer:

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { InsightCard, TitleText, TypingText } from '../components';
-import { Insights } from '../constants';
+import { insights } from '../constants';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -15,7 +15,19 @@ const Insights = () => (
       whileInView="show"
       viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
-    ></motion.div>
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title="Insight about metaverse" textStyles="text-center" />
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
